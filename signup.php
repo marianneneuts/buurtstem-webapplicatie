abf12adb36858690146b2c5db452bba3431640ef
Marianne <marianneneuts@gmail.com>
2022-05-01 19:24:53 +0200

    feat: add the base html on the signup form

--- signup.php
+++ signup.php
@@ -7,6 +7,76 @@
     <title>Buurtstem - Signup</title>
 </head>
 <body>
-    
+    <!-- left side -->
+    <div class="split left">
+        <!-- Buurtstem logo -->
+    </div>
+
+    <!-- right side -->
+    <div class="split right">
+        <div class="BuurtstemSignup">
+            <div class="form-signup">
+                <form action="" method="post">
+
+                    <h2>Registreer je bij Buurtstem!</h2>
+                    <p class="sentence">Maak een account om ideeën, bekommernissen of voorstellen achter te laten omtrent de vernieuwing van de Mechelse Vesten Brusselpoort en Ragheno.</p>
+
+                    <!-- errors -->
+                    <?php if(isset($error)): ?>
+                        <div class="form-error">
+                            <p><strong>Opgepast:</strong></p>
+                            <?php if(isset($error)) { echo $error; }?>
+                        </div>
+                    <?php endif; ?>
+
+                    <!-- voornaam -->
+                    <div class="form__field">
+                        <input type="text" id="voornaam" name="firstname" placeholder="Voornaam">
+                    </div>
+
+                    <!-- achternaam -->
+                    <div class="form__field">
+                        <input type="text" id="achternaam" name="lastname" placeholder="Achternaam">
+                    </div>
+
+                    <!-- email -->
+                    <div class="form__field">
+                        <input type="text" id="email" name="email" placeholder="Email">
+                    </div>
+
+                    <!-- wachtwoord -->
+                    <div class="form__field">
+                        <input type="password" id="wachtwoord" name="password" placeholder="Wachtwoord">
+                    </div>
+
+                    <p class="sentence">Op basis van jouw woonplaats bekijken we welke Mechelse Vest jij mag inrichten!</p>
+
+                    <!-- straatnaam -->
+                    <div class="form__field">
+                        <input type="text" id="straatnaam" name="streetname" placeholder="Straatnaam">
+                    </div>
+
+                    <!-- huisnummer -->
+                    <div class="form__field">
+                        <input type="text" id="huisnummer" name="number" placeholder="Huisnummer">
+                    </div>
+
+                    <!-- plaats -->
+                    <div class="form__field">
+                        <input type="text" id="plaats" name="place" placeholder="Plaats">
+                    </div>
+
+                    <!-- btn -->
+                    <div class="form__field">
+                        <input type="submit" value="Registreren" class="btn-registreren">
+                    </div>
+
+                    <!-- login -->
+                    <p class="login">Heb je al een Buurtstem-account? <a href="login.php" target="_blank"><strong>Aanmelden</strong></a></p>
+
+                </form>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
