b6d28eedd46ca3cbe63a68aaba31c8c0d5358f22
Marianne <marianneneuts@gmail.com>
2022-05-02 09:08:26 +0200

    feat: add the base html on the login form

--- login.php
+++ login.php
@@ -7,6 +7,49 @@
     <title>Buurtstem - Login</title>
 </head>
 <body>
-    
+    <!-- left side -->
+    <div class="split left">
+        <!-- Buurtstem logo -->
+    </div>
+
+    <!-- right side -->
+    <div class="split right">
+        <div class="BuurtstemLogin">
+            <div class="form-login">
+                <form action="" method="post">
+
+                    <h2>Meld je aan bij Buurtstem!</h2>
+                    <p class="sentence">Meld je aan om ideeën, bekommernissen of voorstellen achter te laten omtrent de vernieuwing van de Mechelse Vesten Brusselpoort en Ragheno.</p>
+
+                    <!-- errors -->
+                    <?php if(isset($error)): ?>
+                        <div class="form-error">
+                            <p><strong>Opgepast:</strong></p>
+                            <?php if(isset($error)) { echo $error; }?>
+                        </div>
+                    <?php endif; ?>
+
+                    <!-- email -->
+                    <div class="form__field">
+                        <input type="text" id="email" name="email" placeholder="Email">
+                    </div>
+
+                    <!-- wachtwoord -->
+                    <div class="form__field">
+                        <input type="password" id="wachtwoord" name="password" placeholder="Wachtwoord">
+                    </div>
+
+                    <!-- btn -->
+                    <div class="form__field">
+                        <input type="submit" value="Aanmelden" class="btn-aanmelden">
+                    </div>
+
+                    <!-- signup -->
+                    <p class="signup">Heb je nog geen Buurtstem-account? <a href="signup.php" target="_blank"><strong>Registreren</strong></a></p>
+
+                </form>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
