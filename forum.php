1670d196e8fb46a9fb12ddfe3e74c73c13522797
Marianne <marianneneuts@gmail.com>
2022-05-03 18:48:55 +0200

    feat: include the navigation bars on all pages

--- forum.php
+++ forum.php
@@ -7,8 +7,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Buurtstem - Forum</title>
+    <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
 </head>
 <body>
+    <?php include_once("nav.inc.php"); ?>
     
 </body>
 </html>
