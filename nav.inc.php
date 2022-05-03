17566b93caf81161e51c8784150488bb3658148e
Marianne <marianneneuts@gmail.com>
2022-05-03 18:39:08 +0200

    feat: create navigation bars

---
+++ nav.inc.php
@@ -0,0 +1,36 @@
+<link rel="stylesheet" href="css/navigation.css?v=<?php echo time(); ?>">
+
+<div class="wrapper">
+    <!-- top navigation -->
+    <div class="topbar">
+        <div class="search">
+            <input type="text" name="search" placeholder="Zoeken..." >
+            <button type="submit"><i class="fas fa-search"></i></button>
+        </div>
+
+        <ul>
+            <li>
+                <a href="#" class="profile"><i class="fas fa-user" style="color: #C78743;"></i> Profiel</a>
+            </li>
+        </ul>
+    </div>
+
+    <!-- side navigation -->
+    <div class="sidebar">
+        <div class="home">
+            <h2><a href="forum.php">Buurtstem</a></h2>
+        </div>
+
+        <ul>
+            <li>
+                <a href="#" class="forum"><i class="fas fa-comments" style="color: #C78743;"></i> Forum</a>
+            </li>
+            <li>
+                <a href="#" class="groups"><i class="fas fa-users" style="color: #C78743;"></i> Groepen</a>
+            </li>
+            <li>
+                <a href="#" class="logout"><i class="fas fa-sign-out-alt" style="color: #C78743;"></i> Afmelden</a>
+            </li>
+        </ul>
+    </div>
+</div>
